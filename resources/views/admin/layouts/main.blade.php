21146812059a7053ed3ba51746c5e6078b934055
pyakurelprajal <pyakurelprajal@gmail.com>
2024-09-05 15:18:42 +0545

    cashflow issue fixed

--- resources/views/admin/layouts/main.blade.php
+++ resources/views/admin/layouts/main.blade.php
@@ -32,7 +32,7 @@
     <link href="https://fonts.bunny.net/css?family=Nunito" rel="stylesheet">
 
     <!-- Scripts -->
-    {{-- @vite(['resources/sass/app.scss', 'resources/js/app.js']) --}}
+    @vite(['resources/sass/app.scss', 'resources/js/app.js'])
     <!-- Include Bootstrap CSS (if not already included) -->
 <link href="https://stackpath.bootstrapcdn.com/bootstrap/5.1.0/css/bootstrap.min.css" rel="stylesheet">
 
